2724af13f38744415eaa74b124318f30d74581df
Wenbin Luo <wenbinluo@126.com>
2019-12-10 11:17:19 +0800

    Update start.py

--- traderslink/start.py
+++ traderslink/start.py
@@ -27,15 +27,17 @@ with open(puyuan_config_file) as fin:
         docker_compose_obj['services'][docker_name] = item['docker_compose']
         docker_compose_obj['services'][docker_name]['tty'] = True
         curr_working_dir = os.path.abspath(os.path.join(working_dir, docker_name))
-        docker_compose_obj['services'][docker_name]['volumes'] = [curr_working_dir + ':/shared']
         log_dir = os.path.join(curr_working_dir, 'log')
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
         cfg_dir = os.path.join(curr_working_dir, 'etc')
         if not os.path.exists(cfg_dir):
             os.makedirs(cfg_dir)
-        with open(os.path.join(cfg_dir, 'config.json'), 'w') as fout:
+        cfg_file = os.path.join(cfg_dir, 'config.json')
+        with open(cfg_file, 'w') as fout:
             json.dump(item['config'], fout, indent=4)
+        docker_compose_obj['services'][docker_name]['volumes'] = [log_dir + ':/shared/log']
+        docker_compose_obj['services'][docker_name]['volumes'].append(cfg_file + ':/shared/etc/config.json')
 
 docker_compose_file = 'docker-compose.yml'
 with open(docker_compose_file, 'w') as fout:
