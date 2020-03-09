bd5fed4f7fc24af320ccb6e9baf39d8aedfeca0a
SiTao W_ang <playgoobug@gmail.com>
2020-03-09 14:27:49 +0800

    update md_collection (#16)

--- md_collection/influxdb/scripts/init-influxdb.sh
+++ md_collection/influxdb/scripts/init-influxdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 database_name="trader"
 docker exec dtl-influxdb influx -execute "create database $database_name"
-docker exec dtl-influxdb influx -execute "create retention policy \"2_day\" on $database_name duration 2d replication 1 default"
+docker exec dtl-influxdb influx -execute "create retention policy \"1_day\" on $database_name duration 1d replication 1 default"
